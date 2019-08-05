c2cce4113ee8dbc85a9d26cf9119c00cee7743bf
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-08-05 14:40:19 -0500

    Have a todo list rendered onto page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList.js'
+
+const todoList = [
+  {
+    task: 'Prepare from TK',
+    id: 1,
+    completed: false
+  },
+  {
+    task: 'Follow guided project',
+    id: 2,
+    completed: false
+  },
+  {
+    task: 'Work on Project',
+    id: 3,
+    completed: false
+  },
+]
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  constructor(){
+    super();
+    this.state = {
+      todo: todoList
+    };
+  }
+
+  toggleTask = id => {
+    this.setState ({
+      todo: this.state.todo.map(task => {
+        if (task.id === id){
+          return {
+            ...task,
+            completed: !task.completed
+          }
+        }
+      })
+    })
+  }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <div className="header">
+          <h2>Todo List</h2>
+        </div>
+        <TodoList
+        todo = {this.state.todo}
+        />
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,20 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from 'react';
+import Item from './Item.js'
+
+const TodoList = props => {
+    return (
+        <div className="shopping-list">
+            {props.todo.map(item => (
+                <Item key={item.id} item={item} toggleItem={props.toggleItem} />
+            ))}
+            <button className="clear-btn" onClick={props.clearPurchased}>
+                Clear Purchased
+      </button>
+        </div>
+    );
+};
+
+export default TodoList;
